917dcffcb12c4cb08db990b22e53bcbd8e68092c
ibnss2000 <79970721+ibnss2000@users.noreply.github.com>
2021-07-17 19:05:05 +0700

    Update u1361081_sambongshop.sql

--- u1361081_sambongshop.sql
+++ u1361081_sambongshop.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `u1361081_tokobuah`
+-- Database: `u1361081_sambongshop`
 --
 
 -- --------------------------------------------------------
