28c5c7bb73e1b8f632dc4ce52403a0707ec798de
ibnss2000 <ibnss2000@gmail.com>
2021-07-17 17:29:19 +0700

    ibnu74

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","u1361081_azzam","azzam12345","u1361081_tokobuah");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
